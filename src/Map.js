f30adc696165c91be4349ef013cb0304cb16f14a
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-02-11 15:10:54 -0200

    create a single instance of InfoWindow, whose content'll match that of the clicked marker. Also, if anywhere else on the map clicked, the InfoWindow will be closed

--- src/Map.js
+++ src/Map.js
@@ -2,40 +2,50 @@ import React, { Component } from 'react';
 
 class Map extends Component {
 
+  /**
+   * Loads Google Maps API, creates an instance of a map and an InfoWindow.
+   */
   loadAPI() {
+
+    // Loads API and creates a map.
     const map = new window.google.maps.Map(
       document.getElementById(this.props.id),
       this.props.options);
-    this.loadMarkers(map)
+    // Create only one instance of the InfoWindow, whose values will change as each marker is clicked.
+    const infowindow = new window.google.maps.InfoWindow()    
+    this.loadMarkers(map, infowindow)
   }
 
   /**
-   * Iterate over locations and add markers to them
+   * Iterate over locations and add markers to them.
+   * Each marker has a click event that, when triggered, will change the InfoWindow's content.
    */
-  loadMarkers(map) {
+  loadMarkers(map, infowindow) {
     this.props.locations.map((location) => {
       const marker = new window.google.maps.Marker({
       position: { lat: location.lat, lng: location.lng },
       map: map,
       title: location.title
-      })    
-    this.infoWindows(map, marker, location)
+      })
+      // Method call adds Listener for changing InfoWindow's content upon click
+      this.listenInfoWindowChange(map, marker, location, infowindow)
     })
   }
 
   /**
-   * Adds a InfoWindow, with data passed to it
+   * Changes InfoWindow's content when that marker is clicked.
    * @todo style and apply reverse geocoding, to retrieve the lat/lng correspondent's address
+   * @todo reverse geocoding request (fetch?) to receive `formatted_address`, 
+   * e.g.: https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452
+&location_type=ROOFTOP&result_type=street_address&key=YOUR_API_KEY
    */
-  infoWindows(map, marker, location) {
-    const infowindow = new window.google.maps.InfoWindow({
-      content:
-      `<div>${location.title}</div>
-      <div>${location.description}</div>
-      <div>Address(reverse geocoding - Learn API)</div>`
+  listenInfoWindowChange(map, marker, location, infowindow) {
+    marker.addListener('click', () => {
+      infowindow.setContent(`content: <div>${location.title}</div><br><div>${location.description}</div><br><div>Address(reverse geocoding - Learn API)</div>`)
+      infowindow.open(map, marker)
+      // Closes the InfoWindow if clicked anywhere else on the map.
+      map.addListener('click', () => infowindow.close())
     })
-    marker.addListener('click', () => infowindow.open(map, marker))
-
   }
 
   componentDidMount() {
