09193504cb3e3a7ec2d42e175ac2d3c2d9f8fcaf
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-02-13 13:24:09 -0200

    completely update @todo comments

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 /**
  * # TODOS - [PROJECT SPECIFICATION](https://review.udacity.com/#!/rubrics/1351/view):
+ * @todo 
  * ## Interface Design
- * @todo Make application responsive on anydevice
+ *   --> Make application responsive on anydevice
+ * @todo 
  * ## Application Functionality
- * #### Location Filter
- * @todo text input that filters as the user types, displaying results on the view (restricting markers?)
- * #### List View
- * @todo _1° Requirement_: USE THE BULMA CSS FRAMEWORK! create a list view, whose state starts with all locations, but, when results are filtered, shows only these results. Mobile First! Maybe use an hamburger menu from [Bulma](https://bulma.io/documentation/components/dropdown/)
- * @todo _1° Requirement_: [should I update classes using state, or make components with conditional rendering?](https://stackoverflow.com/questions/36403101/toggle-class-in-react/36404061)
- * @todo _2° Requirement_: When a location in the List-view is clicked, it triggers it's marker's animation and infowindow
+ *   #### Location Filter
+ *     text input that filters as the user types, displaying results on the view (restricting markers?)
+ *   #### List View
+ *    --> _1° Requirement_: USE THE BULMA CSS FRAMEWORK! create a list view, whose state starts with all locations, but, when results are filtered, shows only these results. Mobile First! Maybe use an hamburger menu from [Bulma](https://bulma.io/documentation/components/dropdown/)
+ *    --> _1° Requirement_: [should I update classes using state, or make components with conditional rendering?](https://stackoverflow.com/questions/36403101/toggle-class-in-react/36404061)
+ *    --> _2° Requirement_: When a location in the List-view is clicked, it triggers it's marker's animation and infowindow
  * @todo
+ * ## Accessibility
+ *   add/modify semantic elements and, subsidiarily, `aria roles`
  * @todo
+ * ## Offline Use
+ *   add working service worker, to have offline content when there's no wifi
  * @todo
+ * ## New Todo
+ *   #### New Subtask
+ *      --> 
  * @todo
- * @todo
- * @todo
- * @todo
- * @todo upon project submission, leave note for the reviewer: 
+ * ## FINISH
+ *   --> comment the whole thing
+ *   --> complete README
+ *     --> Credits due:
+ *       - [Traversy Media' Youtube Channel for Crash Course on Bulma CSS Framework](https://www.youtube.com/watch?v=IiPQYQT2-wg)
+ *   --> review rubric
+ *   --> upon project submission, leave `note` for the reviewer: 
     ```
     Understood that could just hardcode information about locations, instead of making API requests to services like Square Space, due to project specification in `Application Architecture`: `There are at least 5 locations in the model. These **may be hard-coded** **_or_** retrieved from a data API.`
     ```
