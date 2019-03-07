b7fce2a4031ad12cd1b02d9e066829abe31d5028
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-03-07 19:42:31 -0300

    set max-height to header

--- src/Filter.js
+++ src/Filter.js
@@ -2,39 +2,65 @@
  * @file Filter.js
  */
 
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 /**
  * Searchbox (for desktop viewport) and datalist (for mobile).
  * @class
  */
 class Filter extends Component {
-	
-	render() {
+  render() {
+    const { query, onFilter } = this.props;
 
-		const { query, onFilter } = this.props
-
-		// div.is-hidden-desktop: Mobile viewport's searchbox/datalist.
-		// input.is-hidden-touch: Desktop viewport's searchbox.
-		return (
-			<div tabIndex="-1">
-				<header className="column navbar is-primary">
-					<div className="is-hidden-desktop" aria-live="assertive" aria-atomic="true" >
-						<input onFocus={() => this.props.handleDatalistFocus()} onBlur={() => this.props.handleDatalistBlur()} defaultValue="" value={query} onChange={(event) => onFilter(event.target.value)} placeholder="Input is case sensitive" list="myLocations" id="location-choice" name="location-choice" aria-label="Search locations (Case Sensitive" />
-						<datalist id="myLocations" style={{"margin":"auto"}}>
-						{
-							this.props.locations.map(location => <option key={location.referralId + location.venue.name}>{location.venue.name}</option>)
-						}
-						</datalist>
-					</div>
-					<input className="is-hidden-touch" defaultValue="" onFocus={() => this.props.handleDatalistFocus()} onBlur={() => this.props.handleDatalistBlur()} value={query} onChange={(event) => onFilter(event.target.value)} placeholder="Input is case sensitive" type="text" name="location-choice" aria-label="Search Locations (Case Sensitive" />
-				</header>
-			</div>
-		)
-	}
+    // div.is-hidden-desktop: Mobile viewport's searchbox/datalist.
+    // input.is-hidden-touch: Desktop viewport's searchbox.
+    return (
+      <div tabIndex="-1">
+        <header className="column navbar is-primary">
+          <div
+            className="is-hidden-desktop"
+            aria-live="assertive"
+            aria-atomic="true"
+          >
+            <input
+              onFocus={() => this.props.handleDatalistFocus()}
+              onBlur={() => this.props.handleDatalistBlur()}
+              defaultValue=""
+              value={query}
+              onChange={event => onFilter(event.target.value)}
+              placeholder="Input is case sensitive"
+              list="myLocations"
+              id="location-choice"
+              name="location-choice"
+              aria-label="Search locations (Case Sensitive"
+            />
+            <datalist id="myLocations" style={{ margin: 'auto' }}>
+              {this.props.locations.map(location => (
+                <option key={location.referralId + location.venue.name}>
+                  {location.venue.name}
+                </option>
+              ))}
+            </datalist>
+          </div>
+          <input
+            className="is-hidden-touch"
+            defaultValue=""
+            onFocus={() => this.props.handleDatalistFocus()}
+            onBlur={() => this.props.handleDatalistBlur()}
+            value={query}
+            onChange={event => onFilter(event.target.value)}
+            placeholder="Input is case sensitive"
+            type="text"
+            name="location-choice"
+            aria-label="Search Locations (Case Sensitive"
+          />
+        </header>
+      </div>
+    );
+  }
 }
 
 /**
  * @exports Filter
  */
-export default Filter
+export default Filter;
